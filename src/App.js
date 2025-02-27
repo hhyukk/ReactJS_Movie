2421b0ac828f52bbf65c4a6d5c18ef74d80b45e5
hhyukk <hhyukk1273@gmail.com>
2025-02-27 14:31:14 +0900

    To Do List

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-function Hello() {
-  useEffect(() => {
-    console.log('Hi :)');
-    return () => console.log('bye :(');
-  }, []);
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, seteShowing] = useState(false);
-  const onClick = () => seteShowing((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [...currentArray, toDo]);
+    setToDo('');
+  };
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
