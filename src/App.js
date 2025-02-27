bd7f6ea10d598a56204b1ec7b66e457696cdba1a
hhyukk <hhyukk1273@gmail.com>
2025-02-27 17:14:16 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo('');
-  };
-  const deleteBtn = (index) => {
-    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState(0);
+  const [coinMoney, setCoinMoney] = useState(0);
+  const onChange = (event) => setMyMoney(event.target.value);
+  const SetCoinMoney = (event) => setCoinMoney(event.target.value);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteBtn(index)}>❌</button>
-          </li>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <input value={myMoney} onChange={onChange} type="text"></input>
+      <select onChange={SetCoinMoney}>
+        <option>Select Coin</option>
+        {coins.map((coin, index) => (
+          <option key={index} value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>
         ))}
-      </ul>
+      </select>
+      <h2>Coin Buy Count: {coinMoney > 0 ? myMoney / coinMoney : null}</h2>
     </div>
   );
 }
